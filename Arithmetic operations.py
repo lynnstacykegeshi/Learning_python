1727c75fbcdd4e236fa2548404220308a6ff600b
lynnstacykegeshi <lynnstacykegeshi@gmail.com>
2023-02-20 19:35:30 +0300

    Arithmetic Operations

---
+++ Arithmetic operations.py
@@ -0,0 +1,31 @@
+print(10+3)
+print(10-3)
+print(10/3)
+print(10*3)
+print(10//3) #returs an integer
+print(10**3) #to the power of
+print(10%3) #returns remainder
+
+#Augmented assignmnent operator
+x=10
+print(x+3)
+#Augmented assignment operator rewrites above code in shorter form
+x += 6
+print(x)
+y=7
+y -=27
+print(y)
+
+#Pythons follows BODMAS rules
+print(2/10 * (6+8*3-2)+ (1/5 + 7/25 - (3/7 + 7/14)))
+
+#Maths functions
+z=-2.78
+print(round(z))
+print(abs(z))
+
+#A Python module is a file containing Python definitions and statements. A module can define functions, classes, and variables.
+#Math module
+import math
+print(math.floor(z))
+print(math.ceil(z))
